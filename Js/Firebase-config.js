2c48f848d895e9521a3a47ac480fdaf9ec606ec1
itxdharmesh <dharmeshsingh4851@gmail.com>
2026-06-19 23:20:10 +0530

    Update Firebase-config.js

--- Js/Firebase-config.js
+++ Js/Firebase-config.js
@@ -1,22 +1,22 @@
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBROVVwRZfnPJcBy1y1jwkG3D124m7S7zw",
-  authDomain: "chronox-4562a.firebaseapp.com",
-  projectId: "chronox-4562a",
-  storageBucket: "chronox-4562a.firebasestorage.app",
-  messagingSenderId: "88388929062",
-  appId: "1:88388929062:web:ec830cab64b8b207dac8e7",
-  measurementId: "G-LMWYZ8RSKN"
+  apiKey: "AIzaSyAIcmtV131UOy8n-t2_NFWFtnXzqHM09EU",
+  authDomain: "chronox-94ad7.firebaseapp.com",
+  databaseURL: "https://chronox-94ad7-default-rtdb.firebaseio.com",
+  projectId: "chronox-94ad7",
+  storageBucket: "chronox-94ad7.firebasestorage.app",
+  messagingSenderId: "571679909717",
+  appId: "1:571679909717:web:45841500518af27258f1dc",
+  measurementId: "G-J99TEGGSST"
 };
-
-// Google Gemini API (Optional - for real AI)
-const GEMINI_API_KEY = "AQ.Ab8RN6JeA-wi9QbkIrCgUYAmOaIRQE3w9t7SJ2Kuy8PrWr3O5g";
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
+
+// Google Gemini API (Optional - for real AI)
+const GEMINI_API_KEY = "AQ.Ab8RN6JeA-wi9QbkIrCgUYAmOaIRQE3w9t7SJ2Kuy8PrWr3O5g";
 
 // Global Variables
 let currentUser = null;
