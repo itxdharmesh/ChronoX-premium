a761530d2389b236aa464586230311b73dedf3ff
itxdharmesh <dharmeshsingh4851@gmail.com>
2026-06-23 09:02:38 +0530

    Refactor service worker caching strategy

--- sw.js
+++ sw.js
@@ -1,26 +1,18 @@
-const CACHE_NAME = 'chronox-v1';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/js/config.js',
-    '/js/utils.js',
-    '/js/auth.js',
-    '/js/app.js'
-];
-
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(ASSETS);
+        caches.keys().then(function(keys) {
+            return Promise.all(keys.map(function(key) { return caches.delete(key); }));
         })
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+    event.respondWith(fetch(event.request).catch(function() {
+        return caches.match(event.request);
+    }));
 });
